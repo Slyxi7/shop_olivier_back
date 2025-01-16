0f83412d5d7521b0b33b297cbf06618df0b789a6
Slyxi7 <riem.baptiste0705@gmail.com>
2025-01-16 16:03:15 +0100

    Connexion a la bdd

--- routes/products.js
+++ routes/products.js
@@ -2,6 +2,8 @@ const { config } = require("dotenv");
 
 const router = require("express").Router();
 const connection = require("../database/configDB")
+// creation de la route pour recuperer toutes les données de la table products
+// url : http//loclahost:5000
 
 router.get('/', (req, res) =>{
     const sql = "SELECT * FROM products"
